ad504111f5ed611a1eecbdd1532fa087bbd49a99
murali_007 <muraliragu99@gmail.com>
2023-06-22 18:28:05 +0530

    new_chg

--- src/App.js
+++ src/App.js
@@ -14,46 +14,22 @@ setDev(check);
 
   return (
 
-  <>
-{
-  dev===1?<>
+  <>    
+<>
   
   <Nav />
    
    <div>
-   <Hero />
+   <Hero /> 
    </div>
    <div className=''>
-   <Skills />
-   </div>
+   <Skills /></div>
    <div className=''>
-<Projects />
+    <Projects /> 
    </div>
   </>
-  :
-  
-  <>
-  <div className='' style={{background:"black",height:"500px",width:"100%"}}>
-
-<div className='h-100 d-flex align-items-center justify-content-center'>
-  <h1 className='text-white'>
-    Under Development!
-  </h1>
-  
-  <div className=''>
-    <br />
-    <br />
-    <br />
-
-  <button class="btn-primary" type="submit" onClick={()=>{handleClick(1)}}>It's OK</button>
-  </div>
-</div>
-  </div>
-  </>
-}
-   
 
    </>
   )
 }
-export default App
+export default App;
